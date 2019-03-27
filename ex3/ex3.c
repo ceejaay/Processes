b594a5af899b99b35a1a1bd0a792440b0fc74418
Chad Jemmett <chad.jemmett@gmail.com>
2019-03-27 16:14:20 -0600

    Working on ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,6 +9,28 @@
 
 int main(void)
 {
+
+  /* char salutation[20]; */
+  int number;
+  int forking = fork();
+  if (forking < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+    /* this is a child b/c rc == 0 */
+  } else if (forking == 0) {
+    int number = 99;
+     /* char salutation = "Hello"; */
+    printf(" %d from the child.", number);
+
+  } else {
+    int number = 11;
+    waitpid(forking, NULL, 0);
+    /* char salutation = "Goodbye"; */
+    printf(" %d from the Parent.", number);
+
+
+
+  }
     // Your code here
 
     return 0;
